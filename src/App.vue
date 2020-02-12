0e02a986aba7d042048e6877bb48ac87179bf483
mauro16 <mauro.silva.skype@gmail.com>
2020-02-12 11:31:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <Header />
+    <v-container>
+      <router-view></router-view>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  name: "App",
+  components: { Header },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
---
+++ src/components/Load.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <div class="loader-wrap">
+      <div class="pin">
+        <div class="pulse"></div>
+      </div>
+    </div>
+  </v-app>
+</template>
