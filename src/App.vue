f8267806ae988499c4aed62714c4882bc3bd5b97
mauro16 <mauro.silva.skype@gmail.com>
2020-02-14 13:03:04 +0100

    added components home

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,7 @@
 </template>
 
 <script>
-import Load from "@/components/Load.vue";
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
 export default {
-  name: "App",
-  components: { Load, Header, Footer },
   data() {
     return {
       //
